f425b18ffdde121eda69037ef15b4ee59c8e5092
Nick Jordan <njordan1017@gmail.com>
2021-11-24 12:00:57 -0600

    challenge-2: checkpoint 2 done

--- challenge-2-token-vendor/packages/hardhat/deploy/00_deploy_your_token.js
+++ challenge-2-token-vendor/packages/hardhat/deploy/00_deploy_your_token.js
@@ -17,8 +17,7 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   const yourToken = await ethers.getContract("YourToken", deployer);
 
   // Todo: transfer tokens to frontend address
-  // const yourToken = await ethers.getContract("YourToken", deployer);
-  // const result = await yourToken.transfer("0x18fFE4dADcCe63A074Ef9cfe327cAb9AD4Ad9f76", ethers.utils.parseEther("1000") );
+  const result = await yourToken.transfer("0xC061379C6b397c7Bbe8a31Ab62e6C1f45b3ace3B", ethers.utils.parseEther("1000") );
 
   // ToDo: To take ownership of yourContract using the ownable library uncomment next line and add the
   // address you want to be the owner.
